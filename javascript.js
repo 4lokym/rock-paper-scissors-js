67bcc98115007479fadb0ef334e89b2d55d3d0bf
4lokym <mykola.koshilko@gmail.com>
2025-01-12 09:31:50 +0100

    Implement computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-alert("hello");
+//a function that choses the computer's choise
+
+function getComputerChoice(){
+    let ran_number = Math.round(Math.random() *10);
+
+    switch (ran_number % 3){
+        case 0:
+        return "rock";
+        case 1:
+        return "paper";
+        case 2:
+        return "scissors";
+    }
+
+}
+
+alert(getComputerChoice());
+
+
